53368e3948712df50ea05cedbc841c1901fcba09
Addison Nishijima and Katy Henning <katy@>
2016-08-23 15:55:13 -0700

    add html, and js to allow user to start an account make initial deposit and additional deposits and withdrawals

---
+++ README.md
@@ -0,0 +1,40 @@
+# _Bank Account_
+
+#### _A website for a user to make withdrawals and deposits to and from a fictional bank account, August 23 2016_
+
+#### By _**Katy Henning and Addison Nishijima**_
+
+## Description
+
+_A website for a user to make withdrawals and deposits to and from a fictional bank account. Allows set up of a bank account with an initial deposit._
+
+### Specs
+
+* Program offers account registration with initial deposit
+* Program offers a view of current balance
+* program offers deposits to balance
+* program offers withdrawals from balance
+
+## Setup/Installation Requirements
+
+* download from github
+* launch in a browser
+
+## Known Bugs
+
+none.
+
+## Support and contact details
+
+Please contact either Katy Henning or Addison Nishijima
+
+## Technologies Used
+
+Bootstrap
+jQuery
+
+### License
+
+This software is Licensed under the MIT License.
+
+Copyright (c) 2016 **_Katy Henning and Addision Nishijima_**
---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+// business interface
+function BankAccount (initial, name) {
+  this.balance = initial;
+  this.name = name;
+}
+
+BankAccount.prototype.deposit = function(amount) {
+  this.balance += amount;
+}
+
+BankAccount.prototype.withdrawal = function(amount) {
+  this.balance -= amount;
+}
+
+
+
+//ui interface
+var newAccount;
+
+$(document).ready(function(){
+  $("#accountbtn").click(function(event){
+    event.preventDefault();
+
+    var Currentbalance = parseInt($("#ideposit").val());
+    var inputtedName = $("#name").val();
+    $("#balance").text(Currentbalance);
+    newAccount = new BankAccount(Currentbalance, inputtedName);
+    $("#name").val("");
+    $("#ideposit").val("");
+    $("#deposit").val("");
+    $("#withdrawal").val("");
+    alert("Hello " + inputtedName + "! Welcome to Bank of Despaire. We are here to accommadate your pain and suffering.")
+  });
+
+  $("#depositbtn").click(function(event){
+    event.preventDefault();
+    var inputtedDeposit = parseInt($("#deposit").val());
+    newAccount.deposit(inputtedDeposit)
+    $("#balance").text(newAccount.balance);
+    $("#name").val("");
+    $("#ideposit").val("");
+    $("#deposit").val("");
+    $("#withdrawal").val("");
+  });
+
+  $("#withdrawalbtn").click(function(event){
+    event.preventDefault();
+    var inputtedWithdrawal = parseInt($("#withdrawal").val());
+    newAccount.withdrawal(inputtedWithdrawal)
+    $("#balance").text(newAccount.balance);
+    $("#name").val("");
+    $("#ideposit").val("");
+    $("#deposit").val("");
+    $("#withdrawal").val("");
+  });
+
+
+
+});
